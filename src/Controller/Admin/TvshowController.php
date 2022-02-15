e0825fc394cd31af36b27f5c4f1d85794eb1f6e9
github_QUENTINBenoit <benquelm@gmail.com>
2022-02-15 14:13:24 +0100

    feat: Admin => Controller/View {Affichage des infos d'une série partie admin}

--- src/Controller/Admin/TvshowController.php
+++ src/Controller/Admin/TvshowController.php
@@ -2,18 +2,19 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Tvshow;
 use App\Repository\TvshowRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
-#[Route('/admin/tvshow', name: 'admin_tvshow_')]
+#[Route('/admin/tvshow', name: 'admin_tvshow_', requirements: ['id' => '\d+'])]
 class TvshowController extends AbstractController
 {
 
     /**
-     * Méthode affichant la liste des séries administration 
+     * Méthode affichant la liste des séries partie administration 
      *
      * @param TvshowRepository $tvshowRepository
      * @return Response
@@ -23,9 +24,17 @@ class TvshowController extends AbstractController
     {
 
         $listSerie = $tvshowRepository->findAllOrderAlpha();
-        //dd($listSerie);
+        //  dd($listSerie);
         return $this->render('admin/tvshow/index.html.twig', [
             'tvshowadmin' => $tvshowRepository->findAllOrderAlpha(),
+        ]);
+    }
+    #[Route('/{id}',  name: 'show')]
+    public function show(Tvshow $tvshow): Response
+    {
+        \dump($tvshow);
+        return $this->render('admin/tvshow/show.html.twig', [
+            'tvshow' => $tvshow
         ]);
     }
 }
