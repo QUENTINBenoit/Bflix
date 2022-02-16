afd1c482c467d2f1d7af75a1976d075855e59a21
github_QUENTINBenoit <benquelm@gmail.com>
2022-02-16 21:11:34 +0100

    feat : Admin\CharactersController {add Methode show}

--- src/Controller/Admin/CharactersController.php
+++ src/Controller/Admin/CharactersController.php
@@ -8,15 +8,32 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
+#[Route('/admin/characters', name: 'admin_characters_')]
 class CharactersController extends AbstractController
 {
-    #[Route('/admin/characters', name: 'admin_characters')]
+    /**
+     * Méthode affichant la list des personnages
+     *
+     * @param CharacterRepository $characterRepository
+     * @return Response
+     */
+    #[Route('/list', name: 'list')]
     public function list(CharacterRepository $characterRepository): Response
     {
 
 
         return $this->render('admin/characters/list.html.twig', [
             'charactersList' => $characterRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/{id}', name: 'show')]
+    public function detailsCharacter(Character $character): Response
+    {
+        //  \dd($character);
+        return $this->render('amdin/characters/show.html.twig', [
+            'character' => $character,
         ]);
     }
 }
