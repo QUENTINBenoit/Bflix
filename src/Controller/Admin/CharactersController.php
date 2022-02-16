288a3148539141019ad8e52914b5b629bedcaf30
github_QUENTINBenoit <benquelm@gmail.com>
2022-02-16 18:37:51 +0100

    feat : Controller  {add : methode list ==> Admin\CharactersController}

--- src/Controller/Admin/CharactersController.php
+++ src/Controller/Admin/CharactersController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Character;
+use App\Repository\CharacterRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +11,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class CharactersController extends AbstractController
 {
     #[Route('/admin/characters', name: 'admin_characters')]
-    public function index(): Response
+    public function list(CharacterRepository $characterRepository)
     {
+        $characterList = $characterRepository->findAll();
+        dd($characterList);
+
         return $this->render('admin/characters/index.html.twig', [
             'controller_name' => 'CharactersController',
         ]);
