2e07ac643864a6ef07dd5d2b4ab89034161cb177
github_QUENTINBenoit <benquelm@gmail.com>
2022-04-25 22:18:44 +0200

    feat: Api //=> tvshowController { validation des données }

--- src/Controller/Api/TvShowController.php
+++ src/Controller/Api/TvShowController.php
@@ -10,6 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 #[Route('/api/v1/tvshows', name: 'api_tvshows_')]
 class TvShowController extends AbstractController
@@ -49,7 +50,7 @@ class TvShowController extends AbstractController
      * @return void
      */
     #[Route(' ', name: 'add', methods: 'POST')]
-    public function  addShow(Request $request, SerializerInterface $serializerInterface, ManagerRegistry $doctrine)
+    public function  addShow(Request $request, SerializerInterface $serializerInterface, ManagerRegistry $doctrine, ValidatorInterface $validator)
     {
         // Je récupère du texte en Json 
         $JsonData = $request->getContent();
@@ -57,17 +58,31 @@ class TvShowController extends AbstractController
         // Json //=> OBJECT
         $tvshow = $serializerInterface->deserialize($JsonData, Tvshow::class, 'json');
 
-        // J'appelle mon manager pour sauvegarder en BDD
-        $em = $doctrine->getManager();
-        $em->persist($tvshow);
-        $em->flush();
-        // Ensuite je retourne ma réponse au client (Insomnia...)
-        return $this->json(
-            [
-                'message' => 'La série ' . $tvshow->getTitle() . 'a bien été créer'
-            ],
-            201 //=> 201 created 
+        // Je vérifie que tous les critères de validations de l'entié Tvshow son respectés =>(Assert\NotBlank)
+        $errors = $validator->validate($tvshow);
 
-        );
+        if (\count($errors) > 0) {
+            // J'ajoute au moin un erreur détectée
+            $errorsString = (string) $errors;
+            return $this->json(
+                [
+                    'message' => $errorsString
+                ],
+                500 //=>  errors 
+            );
+        } else {
+            // J'appelle mon manager pour sauvegarder en BDD
+            $em = $doctrine->getManager();
+            $em->persist($tvshow);
+            $em->flush();
+            // Ensuite je retourne ma réponse au client (Insomnia...)
+            return $this->json(
+                [
+                    'message' => 'La série ' . $tvshow->getTitle() . 'a bien été créer'
+                ],
+                201 //=> 201 created 
+
+            );
+        }
     }
 }
